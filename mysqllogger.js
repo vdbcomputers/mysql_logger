3b3cf4ad963be6c46ddb980455e8026cb44f18a3
vdbcomputers <info@vdbcomputers.nl>
2025-09-19 10:25:22 +0200

    Update mysqllogger.js

--- mysqllogger.js
+++ mysqllogger.js
@@ -8,18 +8,54 @@
 
 "use strict";
 
+const mysql = require('mysql2');
+
 module.exports.mysqllogger = function (parent) {
     var obj = {};
-    obj.parent = parent; // keep a reference to the parent
-    obj.exports = [
-      "onDesktopDisconnect" // export this function to the web UI
-    ];
-    
-    obj.onDesktopDisconnect = function() {  // this is called when the desktop disconnect button is clicked
-        writeDeviceEvent(encodeURIComponent(currentNode._id));  // mimic what the button does on the device main page to pull up a log
-        Q('d2devEvent').value = Date().toLocaleString()+': '; // pre-fill the date for a timestamp
-        focusTextBox('d2devEvent');
+    obj.parent = parent;
+
+    console.log("[MySQL Logger] geladen");
+
+    // DB pool
+    const db = mysql.createPool({
+        host: '127.0.0.1',
+        user: 'meshuser',
+        password: '43x897G@*E27f4gf2rf',
+        database: 'meshmonitor'
+    });
+
+    function logEvent(type, node, details) {
+        db.query(
+            "INSERT INTO events (event_type, server_name, details, timestamp) VALUES (?,?,?,NOW())",
+            [
+                type,
+                node ? node.name : null,
+                JSON.stringify(details || {})
+            ],
+            (err) => { if (err) console.error("MySQL insert error:", err); }
+        );
     }
-    
+
+    // Wordt aangeroepen bij serverstart
+    obj.server_startup = function () {
+        console.log("[MySQL Logger] Plugin gestart");
+        logEvent('server_start', null, {});
+    };
+
+    // Wordt aangeroepen bij server shutdown
+    obj.server_shutdown = function () {
+        logEvent('server_stop', null, {});
+    };
+
+    // Wordt aangeroepen als een node connect
+    obj.node_connect = function (node) {
+        logEvent('node_connected', node, {});
+    };
+
+    // Wordt aangeroepen als een node disconnect
+    obj.node_disconnect = function (node) {
+        logEvent('node_disconnected', node, {});
+    };
+
     return obj;
-}
+};
